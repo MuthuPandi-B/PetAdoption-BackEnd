e216ec086ae1e1620b5c20d33f67d8465ca9525a
MuthuPandi-B <muthup61@gmail.com>
2024-11-11 19:57:22 +0530

    admin middleware added

--- Routers/applicationRoutes.js
+++ Routers/applicationRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { approveApplication, createApplication, deleteApplication, editApplication, rejectApplication,  } from "../Controllers/applicationController.js";
-import { authMiddleware } from "../Middleware/authMiddleware.js";
+import { adminMiddleware, authMiddleware } from "../Middleware/authMiddleware.js";
 import{ loginUser} from "../Controllers/authController.js";
 const router = express.Router();
 router.post("/login",loginUser);
 router.post("/create",authMiddleware, createApplication);
-router.patch("/approve/:id",authMiddleware ,approveApplication);
-router.patch("/reject/:id",authMiddleware,rejectApplication);
+router.patch("/approve/:id",authMiddleware ,adminMiddleware ,approveApplication);
+router.patch("/reject/:id",authMiddleware,adminMiddleware, rejectApplication);
 router.delete("/delete/:id",authMiddleware, deleteApplication);
 router.put("/edit/:id",authMiddleware ,editApplication);
 export default router;
