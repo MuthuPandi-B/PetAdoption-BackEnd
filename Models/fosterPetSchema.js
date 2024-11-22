c1b88bb3d9fee197afb1b11dfe972849672cab26
MuthuPandi-B <muthup61@gmail.com>
2024-11-22 23:35:05 +0530

    removed

--- Models/fosterPetSchema.js
+++ Models/fosterPetSchema.js
@@ -1,18 +1,4 @@
-// import mongoose from 'mongoose';
 
-// const fosterPetSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   breed: { type: String, required: true },
-//   age: { type: Number, required: true },
-//   medicalHistory: { type: String, required: true },
-//   status: { type: String, enum: ['Pending', 'Approved', 'In Foster Care', 'Returned'], default: 'Pending' },
-//   fosterParent: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-//   shelter: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-//   createdAt: { type: Date, default: Date.now },
-//   updatedAt: { type: Date, default: Date.now },
-// });
-
-// export default mongoose.model('FosterPet', fosterPetSchema);
 import mongoose from 'mongoose';
 
 const fosterPetSchema = new mongoose.Schema({
